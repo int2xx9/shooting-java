f7f89c1026a9cae5d007a319942ab954b9febeb1
int2xx9 <github@int512.net>
2015-01-19 14:35:36 +0900

    print score (keyseq_on)

--- shooting/core/Player.java
+++ shooting/core/Player.java
@@ -268,7 +268,7 @@ public abstract class Player implements MainLoopJob, ShootingObject, KeyListener
 
 			if (shooting.isKeyseqOn()) {
 				g.setColor(Color.RED);
-				g.drawString((getWeapon().isCharged() ? "*" : "-") + damage + "/" + getMaxDamage(), getX()+5, getY()+5);
+				g.drawString((getWeapon().isCharged() ? "*" : "-") + damage + "/" + getMaxDamage() + " " + getScore(), getX()+5, getY()+5);
 			}
 		}
 	}
